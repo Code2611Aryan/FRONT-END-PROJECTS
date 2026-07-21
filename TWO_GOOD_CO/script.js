2a25e0014a3ea35273a4a30fa63dfb8cf886c7c4
Aryan Jaiswal <aryan.2611.jaiswal@gmail.com>
2026-07-22 02:11:19 +0530

    Implemented responsive design for Two Good Co clone

--- TWO_GOOD_CO/script.js
+++ TWO_GOOD_CO/script.js
@@ -1,4 +1,19 @@
-let videobox = document.querySelector(".video-box")
+
+
+ const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(".main"),
+    smooth: true,
+     multiplier: 1, // Scroll speed
+  lerp: 0.08,
+  });
+
+  setTimeout(() => {
+  locoScroll.update();
+}, 500); 
+
+// ADDING THE PLAY BUTTON IN TBE VIDEO OF THE HERO SECTION
+function videoanimation(){
+    let videobox = document.querySelector(".video-box")
 let plybtn = document.querySelector(".play")
 videobox.addEventListener("mouseenter",function(){
     gsap.to(plybtn,{
@@ -18,7 +33,37 @@ videobox.addEventListener("mouseleave",function(){
 
 videobox.addEventListener("mousemove", function(dets){
     gsap.to(plybtn,{
-        left: dets.offsetX-20,
-        top: dets.offsetY-20
+        left: dets.offsetX,
+        top: dets.offsetY
     })
 });
+
+}
+
+videoanimation();
+
+// NOW EDITING THE ANIMATION OF THE MAIN TEXT OF THE HERO SECTION 
+
+function textanimation(){
+    let text = document.querySelectorAll(".text");
+    gsap.from(text,{
+    y:100,
+    opacity:0,
+    delay:0.5,
+    duration:0.9,
+    stagger:0.3
+});
+ gsap.from(".page1 .video-box",{
+    scale:0.9,
+    opacity:0,
+    delay:1.3,
+    duration:0.5,
+});
+
+}
+
+textanimation();
+
+
+
+
